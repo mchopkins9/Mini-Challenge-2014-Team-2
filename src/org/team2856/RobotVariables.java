14ae2a8892c98fbdfbde72a58ac4c5fec660567d
Morgan <Morgan@172.16.1.116>
2014-11-10 18:59:53 -0500

    Initial commit. Created MiniChallengeBot and RobotVariables classes.

---
+++ src/org/team2856/RobotVariables.java
@@ -0,0 +1,10 @@
+package org.team2856;
+
+/**
+ * Class that contains all constants relevant to peripherals mapping and overall
+ * robot function.
+ * @author Morgan
+ */
+public class RobotVariables {
+    
+}

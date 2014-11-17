780762443254769f41e2724c6d6e3a60f9204e12
Reagan Prater <bookgeek890@gmail.com>
2014-11-17 16:51:11 -0500

    Added relavent variables to RobotVariables and added a note in MiniChallengeBot about the arm directions

--- src/org/team2856/RobotVariables.java
+++ src/org/team2856/RobotVariables.java
@@ -6,5 +6,10 @@ package org.team2856;
  * @author Morgan
  */
 public class RobotVariables {
+    public static final int DT_MC_MODULE = 1,
+                            DT_MC_LEFT_CHANNEL = 1,
+                            DT_MC_RIGHT_CHANNEL = 2;
     
+    public static final int ARM_MC_MODULE = 1,
+                            ARM_MC_CHANNEL = 3;
 }
